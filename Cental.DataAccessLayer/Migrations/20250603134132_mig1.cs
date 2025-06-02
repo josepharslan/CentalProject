3fffd5f8484c3b8e3d57f2de2cdd40078b5fc687
Yusuf Arslan <markreddcarter@gmail.com>
2025-06-02 15:36:56 +0300

    Add project files.

---
+++ Cental.DataAccessLayer/Migrations/20250603134132_mig1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cental.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Banners_Brands_BrandId",
+                table: "Banners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Banners_BrandId",
+                table: "Banners");
+
+            migrationBuilder.DropColumn(
+                name: "BrandId",
+                table: "Banners");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BrandId",
+                table: "Banners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banners_BrandId",
+                table: "Banners",
+                column: "BrandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Banners_Brands_BrandId",
+                table: "Banners",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "BrandId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Cental.WebUI/Program.cs
@@ -0,0 +1,51 @@
+using Cental.BusinessLayer.Abstract;
+using Cental.BusinessLayer.Concrete;
+using Cental.DataAccessLayer.Abstract;
+using Cental.DataAccessLayer.Concrete;
+using Cental.DataAccessLayer.Context;
+using Cental.DataAccessLayer.Repositories;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<CentalContext>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+
+builder.Services.AddScoped<IBannerService, BannerManager>();
+builder.Services.AddScoped<IBannerDal, EfBannerDal>();
+
+builder.Services.AddScoped<IBrandService, BrandManager>();
+builder.Services.AddScoped<IBrandDal, EfBrandDal>();
+
+builder.Services.AddScoped(typeof(IGenericDal<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
